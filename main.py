6d7d8ffa24c49f70c296f3711edafeb5a1150fe2
Leonardo <lcat140507@gmail.com>
2025-12-28 20:06:42 +0000

    pedir email e senha

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 def main():
-    print("Olá Rent a Car")
+    print("Bem-vindo à Rent-a-Car (Projeto)")
+    email= input("Digite seu e-mail: ")
+    senha=input("Senha: ")
+    print("(Deixar em branco se for cliente)")
+    print(f"Email: {email}")
+    print(f"Senha: {senha}")
+
+    if email=="ola" and senha=="123":
+        print("login com sucesso!")
+    else:
+        print("login invalido.")
+
+
 
 if __name__ == '__main__':
     main()
