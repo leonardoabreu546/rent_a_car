5ff065e3067f855819c383cd0d16c12dcf33bb17
Leonardo <lcat140507@gmail.com>
2025-12-28 20:15:18 +0000

    criar funçao para mostrar login

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-def main():
+def mostrar_login():
     print("Bem-vindo à Rent-a-Car (Projeto)")
-    email= input("Digite seu e-mail: ")
-    senha=input("Senha: ")
+    email = input("Digite seu e-mail: ")
+    senha = input("Senha: ")
     print("(Deixar em branco se for cliente)")
-    print(f"Email: {email}")
-    print(f"Senha: {senha}")
+    return email,senha
+
+def main():
+    email, senha = mostrar_login()
 
     if email=="ola" and senha=="123":
         print("login com sucesso!")
     else:
         print("login invalido.")
+        mostrar_login()
 
 
 
