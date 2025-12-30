4e2c61e87c5eed6822fb3ea4ffd01c39d6a090f2
Leonardo <lcat140507@gmail.com>
2025-12-30 10:54:40 +0000

    organizar ficheiros

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-def mostrar_login():
-    print("Bem-vindo à Rent-a-Car (Projeto)")
-    email = input("Digite seu e-mail: ")
-    senha = input("Senha: ")
-    print("(Deixar em branco se for cliente)")
-    return email,senha
-
-def mostrar_menu_cliente(email):
-    print("MENU CLIENTE")
-    print(f"Utillizador: {email}")
-    print("O que deseja fazer?")
-    opcao = input("Escolha: \n1-para efetuar reserva \n2-para consultar historico de reservas \n3-para sair\n")
-    return opcao
-
-def mostrar_menu_admin(email):
-    print("PAINEL DE ADMINISTRADOR")
-    print(f"Utillizador: {email}")
-    print("O que deseja fazer?")
-    opcao = input("Escolha: \n1-para gerir frota \n2-para gerir classes \n3-para extrato diário \n4-para estatíticas \n5-para sair\n")
-    return opcao
+from ecras import mostrar_login, mostrar_menu_cliente, mostrar_menu_admin
 
 def main():
 
